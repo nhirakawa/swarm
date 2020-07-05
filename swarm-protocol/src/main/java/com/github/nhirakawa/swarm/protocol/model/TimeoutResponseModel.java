c38bc9eaa8d4757047838b5cf7e268623a92afac
Nick Hirakawa <nickhirakawa@gmail.com>
2020-07-05 11:53:24 -0400

    more proxy

--- swarm-protocol/src/main/java/com/github/nhirakawa/swarm/protocol/model/TimeoutResponseModel.java
+++ swarm-protocol/src/main/java/com/github/nhirakawa/swarm/protocol/model/TimeoutResponseModel.java
@@ -2,6 +2,7 @@ package com.github.nhirakawa.swarm.protocol.model;
 
 import com.github.nhirakawa.immutable.style.ImmutableStyle;
 import com.github.nhirakawa.swarm.protocol.config.SwarmNode;
+import com.google.common.base.Preconditions;
 import java.util.Optional;
 import org.immutables.value.Value;
 
@@ -9,4 +10,15 @@ import org.immutables.value.Value;
 @ImmutableStyle
 public interface TimeoutResponseModel {
   Optional<SwarmNode> getTargetNode();
+  Optional<SwarmNode> getProxyNode();
+
+  @Value.Check
+  default void check() {
+    if (getProxyNode().isPresent()) {
+      Preconditions.checkArgument(
+        getTargetNode().isPresent(),
+        "Must set target node when proxy node is set"
+      );
+    }
+  }
 }
