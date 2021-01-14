9fdb3871f46ad38e4dd2a359a01c779c4b2b0011
Nick Hirakawa <nickhirakawa@gmail.com>
2021-01-13 19:28:39 -0500

    properly close channel

--- swarm-netty/src/main/java/com/github/nhirakawa/swarm/transport/client/SwarmClient.java
+++ swarm-netty/src/main/java/com/github/nhirakawa/swarm/transport/client/SwarmClient.java
@@ -6,6 +6,7 @@ import com.github.nhirakawa.swarm.protocol.config.SwarmNode;
 import com.github.nhirakawa.swarm.protocol.model.BaseSwarmMessage;
 import com.github.nhirakawa.swarm.protocol.protocol.SwarmMessageSender;
 import com.github.nhirakawa.swarm.protocol.util.ObjectMapperWrapper;
+import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.Unpooled;
@@ -15,10 +16,15 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import java.io.Closeable;
+import java.net.BindException;
 import java.net.InetSocketAddress;
 import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SwarmClient implements Closeable, SwarmMessageSender {
+  private static final Logger LOG = LoggerFactory.getLogger(SwarmClient.class);
+
   private final SwarmClientChannelInitializer swarmClientChannelInitializer;
   private final EventLoopGroup eventLoopGroup;
 
@@ -52,23 +58,27 @@ public class SwarmClient implements Closeable, SwarmMessageSender {
 
   @Override
   public void send(BaseSwarmMessage swarmEnvelope) {
-    Channel channel = buildChannel();
-    DatagramPacket datagramPacket = wrapInDatagramPacket(
-      swarmEnvelope.getTo(),
-      swarmEnvelope
-    );
-
     try {
-      channel
-        .writeAndFlush(datagramPacket)
-        .sync()
-        .channel()
-        .closeFuture()
-        .await()
-        .sync();
+      Channel channel = buildChannel();
+      DatagramPacket datagramPacket = wrapInDatagramPacket(
+        swarmEnvelope.getTo(),
+        swarmEnvelope
+      );
+      channel.writeAndFlush(datagramPacket).sync().channel().close().sync();
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(e);
+    } catch (Throwable t) {
+      LOG.error("Caught exception", t);
+
+      if (t instanceof BindException) {
+        LOG.error("Could not bind to port 0", t);
+      } else {
+        LOG.error("Caught exception", t);
+      }
+
+      Throwables.throwIfUnchecked(t);
+      throw new RuntimeException(t);
     }
   }
 
@@ -83,6 +93,7 @@ public class SwarmClient implements Closeable, SwarmMessageSender {
         .sync()
         .channel();
     } catch (InterruptedException e) {
+      LOG.error("Caught exception building channel", e);
       Thread.currentThread().interrupt();
       throw new RuntimeException(e);
     }
