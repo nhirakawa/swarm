e80157946c7d07d745d1605dd7ffff940cf8ad44
Nick Hirakawa <nickhirakawa@gmail.com>
2026-07-11 18:46:30 -0400

    Init clap

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "swarm")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Router,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Router => {
+            println!("Starting IPC router...");
+        }
+    }
+
     Ok(())
 }
